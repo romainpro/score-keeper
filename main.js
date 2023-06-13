109834452faf178be01ac6a67d129bcd66778102
romainpro <romain.casagrande.pro@gmail.com>
2023-06-13 14:21:56 +0200

    format-js

--- main.js
+++ main.js
@@ -1,56 +1,45 @@
 let scorePLayeurOne = document.getElementById(`scorePLayeurOne`);
 let scorePLayeurTow = document.getElementById(`scorePLayeurTwo`);
 
-
 //let maxScore = document.getElementById(`maxScore`).value;
-let displayMaxScore =document.getElementById(`maxScoreNumber`);
+let displayMaxScore = document.getElementById(`maxScoreNumber`);
 
+const btnPlayeurOne = document.getElementById(`btnPlayeurOne`);
+const btnPlayeurTwo = document.getElementById(`btnPlayeurTwo`);
+const btnPlayeurReset = document.getElementById(`btnReste`);
 
-const btnPlayeurOne =document.getElementById(`btnPlayeurOne`);
-const btnPlayeurTwo =document.getElementById(`btnPlayeurTwo`);
-const btnPlayeurReset =document.getElementById(`btnReste`);
+btnPlayeurOne.addEventListener(`click`, addPointPlayeurOne);
+btnPlayeurTwo.addEventListener(`click`, addPointPlayeurTow);
+btnPlayeurReset.addEventListener(`click`, reset);
 
+function addPointPlayeurOne() {
+  let maxScore = document.getElementById(`maxScore`).value;
+  if (Number(scorePLayeurOne.textContent >= maxScore)) {
+    scorePLayeurOne.style.color = `green`;
+  } else {
+    let scorePLayeurOneUp = Number(scorePLayeurOne.textContent) + 1;
+    console.log(maxScore);
+    scorePLayeurOne.textContent = scorePLayeurOneUp;
+  }
+}
 
-btnPlayeurOne.addEventListener(`click`,addPointPlayeurOne);
-btnPlayeurTwo.addEventListener(`click`,addPointPlayeurTow);
-btnPlayeurReset.addEventListener(`click`,reset);
+function addPointPlayeurTow() {
+  let maxScore = document.getElementById(`maxScore`).value;
+  if (Number(scorePLayeurTow.textContent >= maxScore)) {
+    scorePLayeurTow.style.color = `green`;
+  } else {
+    let scorePLayeurTowUp = Number(scorePLayeurTow.textContent) + 1;
+    console.log(scorePLayeurTowUp);
+    scorePLayeurTow.textContent = scorePLayeurTowUp;
+  }
+}
 
-
-
-function addPointPlayeurOne(){
-    let maxScore = document.getElementById(`maxScore`).value;
-        if (Number(scorePLayeurOne.textContent >= maxScore)){
-            scorePLayeurOne.style.color=`green`
-            
-        }else{
-            let scorePLayeurOneUp = Number(scorePLayeurOne.textContent) +1;
-            console.log(maxScore)
-            scorePLayeurOne.textContent = scorePLayeurOneUp;
-            
-
-           
-        }
-};
-
-
-function addPointPlayeurTow(){
-    let maxScore = document.getElementById(`maxScore`).value;
-    if (Number(scorePLayeurTow.textContent >= maxScore)){
-        scorePLayeurTow.style.color=`green`
-    }else{
-        let scorePLayeurTowUp = Number(scorePLayeurTow.textContent) +1;
-        console.log(scorePLayeurTowUp)
-        scorePLayeurTow.textContent = scorePLayeurTowUp; 
-        
-    };
-};
-
-function reset(){
-    scorePLayeurOne.textContent= 0;
-    scorePLayeurTow.textContent= 0;
-    maxScore.value = 0;
-    scorePLayeurOne.style.color = 'black';
-    scorePLayeurTow.style.color = 'black';
+function reset() {
+  scorePLayeurOne.textContent = 0;
+  scorePLayeurTow.textContent = 0;
+  maxScore.value = 0;
+  scorePLayeurOne.style.color = "black";
+  scorePLayeurTow.style.color = "black";
 }
 
 // function displayBtn() {
